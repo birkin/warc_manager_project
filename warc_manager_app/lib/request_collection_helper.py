33421e590ba774eacf91e0ff6a93f2c99676a583
Birkin James Diana <birkin.diana@gmail.com>
2024-12-01 19:11:38 -0500

    refactors build-files API call.

--- warc_manager_app/lib/request_collection_helper.py
+++ warc_manager_app/lib/request_collection_helper.py
@@ -1,7 +1,6 @@
-# File: warc_manager_app/lib/helper.py
 import logging
 import pprint
-from typing import Any, Dict, List, Optional
+from typing import List, Optional
 
 import httpx
 from django.conf import settings
@@ -63,6 +62,8 @@ def get_recent_collections() -> list:
     dummy_data.sort(key=lambda x: x['date'], reverse=True)
     return dummy_data
 
+    ## end def get_recent_collections()
+
 
 def render_alert(
     message: str,
@@ -115,69 +116,15 @@ def get_collection_data(collection_id) -> dict | None:
     Called by views.hlpr_check_coll_id().
     """
     log.debug(f'getting data for collection ID: {collection_id}')
-    url = f'{settings.WASAPI_URL_ROOT}?collection={collection_id}'
-    log.debug(f'url = ``{url}``')
-    auth: httpx.BasicAuth = httpx.BasicAuth(username=settings.WASAPI_USR, password=settings.WASAPI_KEY)
-    client: httpx.Client = httpx.Client(auth=auth)
-    resp: httpx.Response = client.get(url)
-    # log.debug(f'resp.content, ``{resp.content}``')
-    log.debug(f'resp = ``{resp}``')
-    elapsed_time: float = resp.elapsed.total_seconds()
-    log.debug(f'elapsed_time, ``{elapsed_time}`` seconds')
-    log.debug(f'type(elapsed_time), ``{type(elapsed_time)}``')
-    # log.debug(f'resp.__dict__ = ``{pprint.pformat(resp.__dict__)}``')
-    # log.debug(f'resp.content = ``{resp.content}``')
-    log.debug('hereZZ')
-    if resp.status_code == 200:
-        log.debug('200 status, so evaluating json')
-        json_data: dict = resp.json()
-        if json_data.get('count', 0) < 1:
-            log.debug(f'json_data for empty-count response, ``{pprint.pformat(json_data)}``')
-            log.debug('no data found')
-            overview_data = None
-        else:
-            log.debug('data found')
-            # overview_data: dict = parse_collection_data(resp)
-            overview_data: dict = parse_collection_data(resp, client)
+    collection_data_prepper = CollectionDataPrepper(collection_id)
+    initial_collection_data: dict | None = collection_data_prepper.grab_initial_collection_data()
+    if initial_collection_data:
+        collection_data_prepper.get_rest_of_files(initial_collection_data)
+        overview_data = collection_data_prepper.build_overview_dict()
     else:
-        log.debug('non-200 status, so setting overview_data to None')
         overview_data = None
+    log.debug(f'overview_data, ``{overview_data}``')
     return overview_data
-
-
-def parse_collection_data(resp: httpx.Response, client: httpx.Client) -> dict:
-    """
-    Parses collection-data api response.
-    Called by get_collection_data().
-    """
-    log.debug('starting parse_collection_data()')
-    data: dict = resp.json()
-    log.debug(f'data (first 1.5K chars), ``{pprint.pformat(data)[:1500]}``')
-    log.debug(f'data.keys(), ``{data.keys()}``')
-    log.debug(f'data.keys(), ``{pprint.pformat(data.keys())}``')
-    ## set up files holder ------------------------------------------
-    all_files: List[str] = []
-    current_data: Dict[str, Any] = data
-    ## store existing files -----------------------------------------
-    all_files.extend(current_data.get('files', []))
-    log.debug(f'number of files initially, ``{len(all_files)}``')
-    ## loop through the remaining pages using "next" links ----------
-    next_url: Optional[str] = data.get('next')
-    while next_url:
-        response = client.get(next_url)
-        if response.status_code != 200:
-            raise RuntimeError(f'Failed to fetch data from ``{next_url}``: ``{response.status_code}``')
-        current_data = response.json()
-        all_files.extend(current_data.get('files', []))
-        next_url = current_data.get('next')
-    ## go through the files and get the total size ------------------
-    file_count: int = len(all_files)
-    log.debug(f'file_count, ``{file_count}``')
-    log.debug(f'First 5 files: {all_files[:5]}')
-    total_size_in_bytes: int = sum([file['size'] for file in all_files])
-    total_size_gb: float = total_size_in_bytes / (1024**3)
-    data = {'total_size': f'{total_size_gb:.2f} GB', 'item_count': file_count}
-    return data
 
 
 def render_download_confirmation_form(api_data: dict, collection_id: str, csrf_token: str | None) -> str:
@@ -203,30 +150,79 @@ def render_download_confirmation_form(api_data: dict, collection_id: str, csrf_t
     return html_content
 
 
-# def render_download_confirmation_form(api_data: dict, collection_id: str, csrf_token: str | None) -> HttpResponse:
-#     """
-#     Preps html for the download confirmation form.
-#     This is triggered by a previous htmx POST request that gets overview collection data
-#     Called by views.hlpr_check_coll_id().
-#     """
-#     html_content = f"""
-#     <div>
-#         Number of items: {api_data["item_count"]}, Total size of all items: {api_data["total_size"]}
-#     </div>
-#     <form hx-post="/hlpr_initiate_download/" hx-target="#response" hx-swap="innerHTML">
-#         <input type="hidden" name="csrfmiddlewaretoken" value="{csrf_token}">
-#         <input type="hidden" name="coll_id" value="{collection_id}">
-#         <input type="hidden" name="action" value="really_start_download">
-#         <button
-#             class="btn">
-#             Confirm start download
-#         </button>
-#     </form>
-#     """
-#     return HttpResponse(html_content)
-
-
 def start_download(collection_id: str):
     log.debug(f'Starting download for collection ID: {collection_id}')
-    # Dummy implementation
+    ## dummy implementation
     return
+
+
+class CollectionDataPrepper:
+    """
+    Class to prepare collection data for a given collection ID.
+    - Makes the initial API call for the given collection-id.
+    - Inspects the response and makes multiple subsequent "next" calls if necessary.
+    - Builds an overview dict with the total size and number of items.
+    Called by get_collection_data().
+    """
+
+    def __init__(self, collection_id: str):
+        self.url = f'{settings.WASAPI_URL_ROOT}?collection={collection_id}'
+        log.debug(f'url = ``{self.url}``')
+        self.auth: httpx.BasicAuth = httpx.BasicAuth(username=settings.WASAPI_USR, password=settings.WASAPI_KEY)
+        self.client: httpx.Client = httpx.Client(auth=self.auth)
+        self.all_files: List[str] = []
+
+    def grab_initial_collection_data(self) -> dict | None:
+        """
+        Makes the initial request to the collection data API.
+        Called by get_collection_data().
+        """
+        resp: httpx.Response = self.client.get(self.url)
+        elapsed_time: float = resp.elapsed.total_seconds()
+        log.debug(f'elapsed_time, ``{elapsed_time}`` seconds')
+        if resp.status_code == 200:
+            log.debug('200 status, so evaluating json')
+            data: dict = resp.json()
+            if data.get('count', 0) < 1:  # eg, collection-id `19111` returns a 200, but a count of zero
+                log.debug(f'data for empty-count response, ``{pprint.pformat(data)}``')
+                data = None
+        else:
+            log.debug('non-200 status, so setting overview_data to None')
+            data = None
+        return data
+
+    def get_rest_of_files(self, data: dict) -> dict:
+        """
+        Makes as many "next" API-calls as necessary.
+        Called by get_collection_data().
+        """
+        log.debug('starting parse_collection_data()')
+        log.debug(f'data (first 1.5K chars), ``{pprint.pformat(data)[:1500]}``')
+        ## store existing files -----------------------------------------
+        self.all_files.extend(data.get('files', []))
+        log.debug(f'number of files initially, ``{len(self.all_files)}``')
+        ## loop through the remaining pages using "next" links ----------
+        next_url: Optional[str] = data.get('next')
+        while next_url:
+            response = self.client.get(next_url)
+            if response.status_code != 200:
+                raise RuntimeError(f'Failed to fetch data from ``{next_url}``: ``{response.status_code}``')
+            current_data = response.json()
+            self.all_files.extend(current_data.get('files', []))
+            next_url = current_data.get('next')
+        return
+
+    def build_overview_dict(self) -> dict:
+        """
+        Builds the overview dict.
+        Called by get_collection_data().
+        """
+        file_count: int = len(self.all_files)
+        log.debug(f'file_count, ``{file_count}``')
+        log.debug(f'First 5 files: {self.all_files[:5]}')
+        total_size_in_bytes: int = sum([file['size'] for file in self.all_files])
+        total_size_gb: float = total_size_in_bytes / (1024**3)
+        data = {'total_size': f'{total_size_gb:.2f} GB', 'item_count': file_count}
+        return data
+
+    ## end class CollectionDataPrepper
