d2ae1086d00ad74e9dd9d9f83a13d37da0e58e77
Birkin James Diana <birkin.diana@gmail.com>
2024-12-06 22:12:47 -0500

    adds admin code.

--- warc_manager_app/admin.py
+++ warc_manager_app/admin.py
@@ -1,3 +1,25 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import UserProfile
+
+
+## enables the user-profile model's fields to be edited inline with the user model (they're at the bottom)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'User Profile'
+
+
+## custom User admin, incorporating UserProfileInline above
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+
+## re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+## register UserProfile separately -- in case we want to edit it directly
+admin.site.register(UserProfile)
