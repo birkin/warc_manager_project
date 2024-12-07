391fa0bc1e3bd6982773775c82dcc73ddc196d80
Birkin James Diana <birkin.diana@gmail.com>
2024-12-06 23:41:50 -0500

    removes UserProfile from inline-User-editing.

--- warc_manager_app/admin.py
+++ warc_manager_app/admin.py
@@ -5,21 +5,14 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-## enables the user-profile model's fields to be edited inline with the user model (they're at the bottom)
-class UserProfileInline(admin.StackedInline):
-    model = UserProfile
-    can_delete = False
-    verbose_name_plural = 'User Profile'
-
-
-## custom User admin, incorporating UserProfileInline above
+# Remove the UserProfileInline from the custom UserAdmin
 class CustomUserAdmin(UserAdmin):
-    inlines = (UserProfileInline,)
+    pass
 
 
-## re-register UserAdmin
+# Re-register the UserAdmin without the inline
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
-## register UserProfile separately -- in case we want to edit it directly
+# Keep UserProfile registered separately for direct editing
 admin.site.register(UserProfile)
