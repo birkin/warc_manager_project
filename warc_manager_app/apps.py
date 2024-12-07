d68222e38e983b42aa899aec1b0658c834cce338
Birkin James Diana <birkin.diana@gmail.com>
2024-12-06 23:42:11 -0500

    updates apps and signals file.

--- warc_manager_app/apps.py
+++ warc_manager_app/apps.py
@@ -8,12 +8,23 @@ Notes...
   a User record is created.
 """
 
+import logging
+import types
+
 from django.apps import AppConfig
+
+log = logging.getLogger(__name__)
 
 
 class WarcManagerAppConfig(AppConfig):
+    log.debug('starting WarcManagerAppConfig')
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'warc_manager_app'
 
     def ready(self):
-        pass  # this is the reason for this apps.py file
+        log.debug('starting ready()')
+        import warc_manager_app.signals  # this is the reason for this apps.py file
+
+        log.debug(f'type(warc_manager_app.signals): {type(warc_manager_app.signals)}')
+        assert type(warc_manager_app.signals) is types.ModuleType
+        log.debug('done with ready()')
--- warc_manager_app/signals.py
+++ warc_manager_app/signals.py
@@ -4,27 +4,37 @@ Enables auto-creation of a UserProfile record when a new User record is created.
 See the README for more info.
 """
 
+import logging
+
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from warc_manager_app.models import UserProfile
 
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-    else:
-        UserProfile.objects.get_or_create(user=instance)
-        instance.userprofile.save()
+log = logging.getLogger(__name__)
 
 
 # @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 # def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     log.debug('starting create_or_update_user_profile()')
 #     if created:
-#         # Check if a UserProfile already exists to avoid duplication
-#         UserProfile.objects.get_or_create(user=instance)
+#         log.debug('creating a UserProfile record')
+#         UserProfile.objects.create(user=instance)
 #     else:
-#         # Update or save an existing UserProfile
+#         log.debug('updating a UserProfile record')
+#         UserProfile.objects.get_or_create(user=instance)
 #         instance.userprofile.save()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    log.debug('starting create_or_update_user_profile()')
+    if created:
+        log.debug('creating a UserProfile record')
+        # Check if a UserProfile already exists to avoid duplication
+        UserProfile.objects.get_or_create(user=instance)
+    else:
+        log.debug('updating a UserProfile record')
+        # Update or save an existing UserProfile
+        instance.userprofile.save()
