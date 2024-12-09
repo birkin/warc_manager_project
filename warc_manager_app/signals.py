be4a1b8ecec46caaf9b2dfa0eb595cf6b03755a1
Birkin James Diana <birkin.diana@gmail.com>
2024-12-09 06:59:37 -0500

    adds type-hinting to signals.py

--- warc_manager_app/signals.py
+++ warc_manager_app/signals.py
@@ -5,8 +5,10 @@ See the README for more info.
 """
 
 import logging
+from typing import Any, Type
 
 from django.conf import settings
+from django.db.models import Model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -28,7 +30,8 @@ log = logging.getLogger(__name__)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender: Type[Model], instance: Model, created: bool, **kwargs: Any) -> None:
     log.debug('starting create_or_update_user_profile()')
     if created:
         log.debug('creating a UserProfile record')
